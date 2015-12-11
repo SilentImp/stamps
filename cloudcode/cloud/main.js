6bf3067bc48c17b9c9698bf02a24c0d6e2beecee
Anton Nemtsev <thesilentimp@gmail.com>
2015-12-11 11:33:40 +0200

    Update 2015-12-11T09:33:37.685Z

--- cloudcode/cloud/main.js
+++
@@ -1,8 +0,0 @@
-var Stripe = require('stripe');
-Stripe.initialize('mySecretKey');
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
